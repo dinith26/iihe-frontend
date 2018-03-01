9a1dd112eae93fd7412146e260822c28234cbb7e
dinith26 <dinith26@gmail.com>
2018-03-01 11:33:03 +0530

    add new navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
   ],
   imports: [
     BrowserModule
